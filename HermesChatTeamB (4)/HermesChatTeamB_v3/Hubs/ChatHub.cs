26cf8064f02071b39a82da182b9708057114003b
MarijaNet <marija.borisovanet@gmail.com>
2022-02-16 19:44:05 +0200

    SignalR Online/Offline User bug fixed

--- HermesChatTeamB (4)/HermesChatTeamB_v3/Hubs/ChatHub.cs
+++ HermesChatTeamB (4)/HermesChatTeamB_v3/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Threading.Tasks;
 
 namespace HermesChatTeamB_v3.Hubs
@@ -13,6 +14,22 @@ namespace HermesChatTeamB_v3.Hubs
         public Task LeaveRoom(string roomId)
         {
             return Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId);
+        }
+
+        public async Task Send(string message)
+        {
+            await Clients.All.SendAsync("Receive", message, Context.User.Identity.Name);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.User.Identity.Name} is online");
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.User.Identity.Name} left the chat");
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
